b5447a2febfc575a399179fbfadc0643c04dee3f
mfm2139 <mfm2139@columbia.edu>
2018-12-20 21:03:26 -0500

    181220 - V1

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
-const apiKey = 'd3b5cd21'
-let key = 't'
+const apiKey = 'e9c04929'
+let key = 't' //standing for title
 
 let searchBox = document.querySelector('#searchBox')
 let poster = document.querySelector('#poster')
 let title = document.querySelector('#title')
 let actors = document.querySelector('#actors')
-// Creating a variable called 'searchBox'
-// searchBox is assigned to the input search box class
 
+console.log("hi")
+//creating a variable called 'searchbox'
+//searchbox is assigned to the input search box class
 
-//The searchBox object will fire (aka call) a function when the user punches a key on their keyboard
-//'e' refers to an object that contains data from the user (SHORT FOR EVENT OBJECT)
-searchBox.addEventListener('keyup',function(e){
-  //If nothing is typed, return and exit out of the function
+searchBox.addEventListener('keyup', function(e){
+  //if nothing is typed, return and exit ouf of the function
   if(!e.target.value){
-    poster.src=""
-    title.innerHTML=""
-    return
-  }
-
-  //Every time the event listener fires, it will call 'getMovie()'
+  // anbove just means if nothing is typed
+  poster.src=""
+  title.innerHTML=""
+} else {
   getMovie(e.target.value)
+  //every time the event listener fires -- key goes up -- it will call
+  // getMovie, a function we define below
+}
 })
 
-
-
-function getMovie(input){
+function getMovie (input){
+  //the input here is whatever the user put in
+  //this gets called only if the user input something here
+  //below its a template literal; just a way of concatenating strings
+  //defined apiKey above, key above, and then it accepts input from the user
   fetch(`http://www.omdbapi.com/?apikey=${apiKey}&${key}=${input}`)
-  .then(function(response) {
-    return response.json();
+  .then(function(response){
+    return response.json()
   })
-  .then(function(data) {
+  .then(function(data){
     makeMovie(data)
-  });
+  })
 }
 
 function makeMovie(movie){
   console.log(movie)
   if(movie.Error==='Movie not found!'){
     poster.src="images/cantFind.jpg"
-    title.innerHTML = ''
-    return
+    title.innerHTML= ''
+  } else {
+    title.innerHTML=movie.Title
+    actors.innerHTML=movie.Actors
+    poster.src = movie.Poster
   }
-  title.innerHTML = movie.Title
-  actors.innerHTML = movie.Actors
-  poster.src = movie.Poster
-
 }
